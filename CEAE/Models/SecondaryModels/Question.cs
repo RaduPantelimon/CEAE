adf3119d5f24f29406b16f6a689f63bc8c9fa55e
Radu Bucur <Radu Bucur@DESKTOP-ILG51QO>
2017-04-09 18:58:28 +0300

    Added buddy classes for our entity model classes

---
+++ CEAE/Models/SecondaryModels/Question.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using Newtonsoft.Json;
+
+namespace CEAE.Models
+{
+    [MetadataType(typeof(QuestionMD))]
+    public partial class Question
+    {
+    }
+
+    public class QuestionMD
+    {
+        [JsonIgnore]
+        public virtual ICollection<AnswersQuestion> AnswersQuestions { get; set; }
+    }
+}
